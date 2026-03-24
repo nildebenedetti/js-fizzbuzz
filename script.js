ea2150905b441356ad7c16c6d2fbb6edbdd03f35
“Nilde <nilde.benedetti@gmail.com>
2026-03-24 15:12:07 +0100

    aggiunto loop in script.js per stampare i numeri da 1 a 100 in console

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 'strict';
 
-console.log('hello');
+// Stampa i numeri da 1 a 100 --> usiamo for, per far contarte con +1 ad ogni ciclo 
+
+for (let i = 0; i <= 100; i++) { // dichiaro e assegno variabile i con valore 0 per il loop; aggiungi 1 a ogni ciclo fino a che i <=100
+    console.log(i); // stampa i numeri in console
+} 
