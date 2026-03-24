b87d6727de83d2adb4eb4086c015d62e88e8e003
“Nilde <nilde.benedetti@gmail.com>
2026-03-24 15:39:12 +0100

    add if condition at top of if chain for prioritizing execution when running proghram in script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 'strict';
 
-
-
-
-// Stampa i numeri da 1 a 100 --> usiamo for, per far contarte con +1 ad ogni ciclo 
+// Stampa i numeri da 1 a 100
+// usiamo for, per far contarte con +1 ad ogni ciclo ed uscire una volta raggiuno valore 100 ed eseguito il programma
 
 for (let i = 0; i <= 100; i++) { // dichiaro e assegno variabile i con valore 0 per il loop; aggiungi 1 a ogni ciclo fino a che i <=100
     
-    if (i % 3 === 0) {  // se il numero e'multiplo di 3, stampa fizz
+    if (i % 3 === 0 && i % 5 === 0) { // se i e'sia multiplo di 3 che di 5, stampa, al;trimenti prosegui
+        console.log("FizzBuzz");
+
+    }
+
+    else if (i % 3 === 0) {  // se il numero e'multiplo di 3, stampa Fizz
     console.log("Fizz");
 
     }
 
-    else if (i % 5 === 0) {  // se il numero e'multiplo di 5, stampa Buzz
+       else if (i % 5 === 0) {  // se il numero e' multiplo di 5, stampa Buzz
     console.log("Buzz");
 
-    }
-
+       }
 
     else { // altrimenti stampa i
         console.log(i);
