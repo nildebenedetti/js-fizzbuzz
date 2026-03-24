121eefd87bb70b990cc27fc4cb74f91ac17bf2c6
“Nilde <nilde.benedetti@gmail.com>
2026-03-24 15:25:47 +0100

    add if chain in loop for determining output in script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 'strict';
 
+
+
+
 // Stampa i numeri da 1 a 100 --> usiamo for, per far contarte con +1 ad ogni ciclo 
 
 for (let i = 0; i <= 100; i++) { // dichiaro e assegno variabile i con valore 0 per il loop; aggiungi 1 a ogni ciclo fino a che i <=100
-    console.log(i); // stampa i numeri in console
+    
+    if (i % 3 === 0) {  // se il numero e'multiplo di 3, stampa fizz
+    console.log("fizz");
+
+    }
+
+    else { // altrimenti stampa i
+        console.log(i);
+    }
 } 
